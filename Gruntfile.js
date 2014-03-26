fbd344c8e72868c82e74c9281fe815a4f2a82786
John Culviner <john.culviner@gmail.com>
2014-03-25 19:01:03 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,21 +5,21 @@ module.exports = function (grunt) {
         concat: {
             options: {
                 separator: ';'
-            },
-            dist: {
-                src: ['src/**/*.js'],
-                dest: 'dist/<%= pkg.name %>.js'
-            }
+            }//, put back when feature stable
+            //dist: {
+            //    src: ['src/**/*.js'],
+            //    dest: 'dist/<%= pkg.name %>.js'
+            //}
         },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-            },
-            dist: {
-                files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-                }
-            }
+            }//, put back when when feature stable
+            //dist: {
+            //    files: {
+            //        'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+            //    }
+            //}
         },
         jshint: {
             files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
