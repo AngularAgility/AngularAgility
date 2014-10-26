93a7952793e7236ee6fa038337a37384b906c4d2
John Culviner <john.culviner@gmail.com>
2014-10-25 22:41:51 -0500

    Release v0.8.16

--- dist/angular-agility.min.js
+++ dist/angular-agility.min.js
@@ -1,5 +1,5 @@
 /*
-angular-agility "version":"0.8.15" @ 2014-10-25T22:34:28
+angular-agility "version":"0.8.16" @ 2014-10-25T22:34:28
 Copyright (c) 2014 - John Culviner
 Licensed under the MIT license
 */
